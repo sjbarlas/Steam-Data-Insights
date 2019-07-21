f57a9fe6ef5cf9ca1b391217bda14c50813e2c4a
sjbarlas <sairajavedbarlas@gmail.com>
2019-07-21 19:17:06 +0100

    better code spacing

--- Tables_Creation_and_Data_Pull_to_PostgresDB.sql
+++ Tables_Creation_and_Data_Pull_to_PostgresDB.sql
@@ -6,7 +6,8 @@ CREATE TABLE public."achievement_percentages" (
 	ach_name text, percentage numeric
 )
 
-\COPY public."achievement_percentages" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Achievement_Percentages.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."achievement_percentages" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Achievement_Percentages.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 2nd Table: app_id_info
 --- a. Creation of app_id_info table and dimensions
@@ -22,7 +23,8 @@ CREATE TABLE public."app_id_info" (
 	is_multiplayer boolean
 )
 
-\COPY public."app_id_info" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\App_ID_Info.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."app_id_info" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\App_ID_Info.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 3rd Table: friends
 --- a. Creation of friends table and dimensions
@@ -34,7 +36,8 @@ CREATE TABLE public."friends" (
 	dateretrieved timestamp with time zone
 )
 
-\COPY public."friends" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Friends.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."friends" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Friends.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 4th Table: games_1
 --- a. Creation of games_1 table and dimensions
@@ -47,7 +50,8 @@ CREATE TABLE public."games_1" (
 	dateretrieved timestamp with time zone
 )
 
-\COPY public."games_1" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_1.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."games_1" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_1.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 5th Table: games_2
 --- a. Creation of games_2 table and dimensions
@@ -60,7 +64,8 @@ CREATE TABLE public."games_2" (
 	dateretrieved timestamp with time zone
 )
 
-\COPY public."games_2" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_2.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."games_2" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_2.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 6th Table: games_developers
 --- a. Creation of games_developers table and dimensions
@@ -70,7 +75,8 @@ CREATE TABLE public."games_developers" (
 	app_developer text
 )
 
-\COPY public."games_developers" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Developers.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."games_developers" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Developers.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 7th Table: games_genres
 --- a. Creation of games_genres table and dimensions
@@ -80,7 +86,8 @@ CREATE TABLE public."games_genres" (
 	app_genre text
 )
 
-\COPY public."games_genres" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Genres.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."games_genres" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Genres.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 8th Table: games_publishers
 --- a. Creation of games_publishers table and dimensions
@@ -90,7 +97,8 @@ CREATE TABLE public."games_publishers" (
 	app_publisher text
 )
 
-\COPY public."games_publishers" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Publishers.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."games_publishers" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Games_Publishers.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 9th Table: groups
 --- a. Creation of groups table and dimensions
@@ -101,7 +109,8 @@ CREATE TABLE public."groups" (
 	dateretrieved timestamp with time zone
 )
 
-\COPY public."groups" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Groups.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."groups" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Groups.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
 
 --- 10th Table: player_summaries
 --- a. Creation of player_summaries table and dimensions
@@ -130,4 +139,5 @@ CREATE TABLE public."player_summaries" (
 	loccityid integer, 
 	dateretrieved timestamp with time zone)
 										
-\COPY public."player_summaries" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Player_Summaries.csv' WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
+\COPY public."player_summaries" FROM 'C:\Users\Saira\Downloads\steam_gaming_small\steam_gaming_small\Player_Summaries.csv' 
+WITH DELIMITER AS ',' ENCODING 'UTF8' CSV HEADER;
